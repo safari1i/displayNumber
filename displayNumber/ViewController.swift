c45c9c42c4c02c05e9506cf69dcd59d1cbe1199e
LEO CANTU <rl300126@rlas-macair-39.rlas-116.org>
2018-09-25 11:01:00 -0500

    added text field, label, and 3 vertical input boxes. Also ran app

--- displayNumber/ViewController.swift
+++ displayNumber/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onButtonTapped(_ sender: Any) {
+        let numberToDisplay = numberTextField.text
+        numberLabel.text = numberToDisplay
+        
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
+    
 
 }
+
 
